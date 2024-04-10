a4e1e74a2bba30741c99faa7cd79e59b5e8b9e2e
Dmitry Andriyanov <facklak@mail.ru>
2024-04-10 17:39:13 +0300

    feat: did getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -216,8 +216,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let number = startFrom;
+
+  return () => {
+    const currentNumber = number;
+    number += 1;
+    return currentNumber;
+  };
 }
 
 module.exports = {
